15c7eaa8d53c4a74b6737449dd8dfeaaff9ecb40
Abdallahyx <99212200+Abdallahyx@users.noreply.github.com>
2022-10-19 00:29:13 +0200

    Update vgg16.py

--- vgg16.py
+++ vgg16.py
@@ -68,7 +68,7 @@ def VGG16(include_top=True, weights='imagenet',
     inputs = Input(shape=input_shape)
   
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
