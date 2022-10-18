f0958a4f5e7fff1cd8f63d46d70b541c741a98f8
Abdallahyx <99212200+Abdallahyx@users.noreply.github.com>
2022-10-19 00:30:32 +0200

    Update vgg19.py

--- vgg19.py
+++ vgg19.py
@@ -25,8 +25,6 @@ def VGG19(include_top=True, weights='imagenet',
             layers at the top of the network.
         weights: one of `None` (random initialization)
             or "imagenet" (pre-training on ImageNet).
-        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
-            to use as image input for the model.
         input_shape: optional shape tuple, only to be specified
             if `include_top` is False (otherwise the input shape
             has to be `(224, 224, 3)` (with `channels_last` data format)
